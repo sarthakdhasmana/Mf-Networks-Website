7561b937d03a7dd84d1b64b896d669c05642de6e
sarthakdhasmana <sarthakmay09@gmail.com>
2022-01-31 07:44:18 +0530

    added more section, fixed responsive for mobile.

--- js/app.js
+++ js/app.js
@@ -31,18 +31,53 @@ consoleArea.addEventListener('mouseleave', e => {
     gameConsoler.style.transition = 'all 0.7s ease';
 })
 
+// Mouse leave animation for the card--Spec Card
+let specard = document.querySelector('.card1');
+let specard_ = document.querySelector('.card2');
+let specard__ = document.querySelector('.card3');
+specard.addEventListener('mouseleave', e => {
+    specard.style.transition = 'all 0.5s ease';
+    specard_.style.transition = 'all 0.5s ease';
+    specard__.style.transition = 'all 0.5s ease';
+})
 
-//>>>>>>>>>Partner SECTION Carousel>>>>>>>>>>>
-let moveNext = document.querySelector('.carousel-btn-next');
-let movePrev = document.querySelector('.carousel-btn-prev');
-let partner = document.querySelector('.partner-list');
-let partnerpng = Array.from(partner.children);
-let slideWidth = partnerpng[0].getBoundingClientRect().width;
+// Animation for the Game Card
+let projContainer = document.querySelector('.project');
+let projCard = document.querySelector('.project_card');
+let projText = document.querySelector('.project-dec');
+let projImg = document.querySelector('.project-img');
 
+projContainer.addEventListener('mousemove', e => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 50;
+    let yAxis = (window.innerHeight / 2 - e.pageX) / 50;
 
-// let partner;
-let width = 0;
-for (let i = 0; i < 4; i++) {
-    width += partnerpng[i].getBoundingClientRect().width;
-}
-console.log(width);
+    projCard.style.transform = `rotateX(${yAxis}deg) rotateY(${xAxis}deg)`;
+})
+projCard.addEventListener('mouseenter', e => {
+    projCard.style.transition = 'none';
+    projText.style.transform = 'translateZ(50px)';
+    projImg.style.transform = 'translateZ(50px)';
+})
+projContainer.addEventListener('mouseleave', e => {
+    projCard.style.transform = 'rotateX(0deg) rotateY(0deg)';
+    projCard.style.transition = 'all 0.5s ease';
+    projText.style.transform = 'translateZ(0px)';
+    projImg.style.transform = 'translateZ(0px)';
+})
+
+//Carousel for Projects
+let movePrev = document.querySelector('.prev-project');
+let moveNext = document.querySelector('.next-project');
+let currentSlide = document.querySelector('.current-slide');
+let list = document.querySelector('.projects-list');
+let slides = Array.from(list.children);
+console.log(slides)
+let nextSlide = currentSlide.nextSibling;
+
+//move to left when i click
+moveNext.addEventListener('click', e => {
+    moveWidth = currentSlide.getBoundingClientRect().width + 'px';
+    currentSlide.style.transform = `translateX(-${moveWidth})`;
+    currentSlide.classList.remove('current-slide');
+    // nextSlide.classList.add('current-slide');
+})
